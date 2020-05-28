8b4be7bde5b8066b4683312e641e6ed6739afb4f
GesielM <gesiel_02@hotmail.com>
2020-05-27 21:15:18 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,21 @@
 class   Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-        p = Pessoa('Gesiel')
-        print(Pessoa.cumprimentar(p))
-        print(id(p))
-        print(p.cumprimentar())
-        print(p.nome)
-        p.nome = 'Renzo'
-        print(p.nome)
-        print(p.idade)
+        renzo = Pessoa(nome='Renzo')
+        Gesiel = Pessoa(renzo, nome='Gesiel')
+        print(Pessoa.cumprimentar(Gesiel))
+        print(id(Gesiel))
+        print(Gesiel.cumprimentar())
+        print(Gesiel.nome)
+        print(Gesiel.idade)
+        for filho in Gesiel.filhos:
+            print(filho.nome)
+
